e8a3787d6f21ad0c2033c40efbd67d653b382de4
misalibaytb <66683443+misalibaytb@users.noreply.github.com>
2025-07-11 14:31:18 +0200

    Enhance cookie handling by setting domain for cookies to improve cross-subdomain support

--- js/cok.js
+++ js/cok.js
@@ -3,8 +3,14 @@
 
     function setCookie(name, value, days) {
         const expires = new Date(Date.now() + days * 864e5).toUTCString();
-        document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
+        const domain = window.location.hostname
+            .split('.')
+            .slice(-2)
+            .join('.'); // firecone.eu z blog.firecone.eu
+
+        document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/; domain=.${domain}`;
     }
+
 
     function getCookie(name) {
         return document.cookie.split('; ').reduce((r, v) => {
